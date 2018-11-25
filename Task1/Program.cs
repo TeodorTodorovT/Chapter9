80b8cfe75697e1fe7d006af494305baec2bdff84
Teodor Todorov <teodortodorov752@gmail.com>
2018-11-25 18:20:32 +0200

    Solved Chapter 9

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //task: Write a method that by given name prints on the console "Hello, !" (for example: "Hello, Peter!" ).
+
+            Console.Write("Enter name: ");
+            ReturnName(Console.ReadLine());
+            Console.ReadLine();    
+        }
+        static void ReturnName(string name)
+        {
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static int a;
+        static void Main(string[] args)
+        {   //task: Create a method GetMax() with two integer (int) parameters, that returns maximal of the two numbers. Write a program that reads three numbers from the console and prints the biggest of them. Use the GetMax() method you just created.
+            Console.Write("Enter first number: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int c = int.Parse(Console.ReadLine());
+
+            GetMax(a, b);
+            GetMax(a, c);
+
+            Console.WriteLine("Biggest number is {0}", a);
+            Console.ReadLine();
+        }
+        static void GetMax(int first, int second)
+        {
+            if (first > second) a = first;
+            else a = second;
+        }
+    }
+}
